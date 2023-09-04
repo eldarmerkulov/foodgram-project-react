ffc342d9aa0355141ed1ebd459fa45cc232d1bbe
Merkulov <emerkulov@yandex.ru>
2023-09-04 10:46:16 +0300

    nginx

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -141,7 +141,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
