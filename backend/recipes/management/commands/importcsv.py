7293408720a1accebe8f9ca631eac6a52be22d0e
Merkulov <emerkulov@yandex.ru>
2023-09-06 18:49:35 +0300

    v.2

--- backend/recipes/management/commands/importcsv.py
+++ backend/recipes/management/commands/importcsv.py
@@ -3,10 +3,11 @@ import csv
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 MODELS = {
     Ingredient: 'ingredients.csv',
+    Tag: 'tags.csv',
 }
 
 
